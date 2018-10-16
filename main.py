3837e4a2aa7a278dd5efeae769fea64f6857eb7f
Christopher Calmes <christopher.calmes@okbu.edu>
2018-10-16 16:51:03 -0500

    removed delete_file and wrote functions to handle preferences

--- main.py
+++ main.py
@@ -7,28 +7,26 @@ import shutil
 import random
 from PIL import Image
 from os.path import splitext
+import fileinput
+import sys
 
 project_path = os.getcwd()
 nature_folder = project_path + "\\res\\images\\nature\\"
 
 num_nature = 15
+seasonal_backgrounds_line = 123
+beatmap_skin_line = 58
+thumbnail_line = 110
 
 username = str(os.getlogin())
 # default osu installation path
-osu_path = "C:\\Users\\" + username + "\\AppData\\Local\\osu!\\Songs"
-os.chdir(osu_path)
+osu_song_path = "C:\\Users\\" + username + "\\AppData\\Local\\osu!\\Songs"
+os.chdir(osu_song_path)
 
 
 def get_background(folder, ext):
     return folder + str(random.randrange(num_nature)) + ext
     # todo get number of files in folder
-
-    # def delete_original(song_folder):
-    try:
-        os.remove(song_folder)
-    except PermissionError as e:
-        print(song_folder)
-        print('Error: %s' % e.strerror)
 
 
 def replace_file(src, dest):
@@ -68,19 +66,42 @@ def get_preferences():
         get_song_skin = True
     turn_off_thumbnails_str = str(input("Do you want to turn off thumbnails?? Yes/No: "))
     if turn_off_thumbnails_str.lower() == "yes":
-        get_turn_off_thumbnails = False
+        get_turn_off_thumbnails = True
     return get_seasonal, get_song_skin, get_turn_off_thumbnails
+
+
+def change_preferences(config_file, seasonal, song_skin, turn_off_thumbnails):
+    with open(config_file, 'r') as file:
+        # read a list of lines into data
+        data = file.readlines()
+    if seasonal:
+        # deletes final characters and changes setting
+        data[seasonal_backgrounds_line] = data[seasonal_backgrounds_line][:-6] + "Never\n"
+    if song_skin:
+        data[beatmap_skin_line] = data[beatmap_skin_line][:-2] + "1\n"
+    if turn_off_thumbnails:
+        data[thumbnail_line] = data[thumbnail_line][:-2] + "1\n"
+
+    # and write everything back
+    with open(config_file, 'w') as file:
+        file.writelines(data)
 
 
 def main():
     seasonal = False
     song_skin = False
     turn_off_thumnails = False
-    change_preferences = str(input("Do you want to change preferences? Yes/No"))
+    change_preferences_test = str(input("Do you want to change preferences? Yes/No"))
 
-    if change_preferences.lower() == "yes":
-        seasonal, song_skin, turn_off_thumnails = get_preferences()
-    #todo change preferences
+    # go up a directory to change file in root colder
+    os.chdir('..')
+    if change_preferences_test.lower() == "yes":
+        seasonal, song_skin, turn_off_thumbnails = get_preferences()
+        change_preferences("osu!." + username + ".cfg", seasonal, song_skin, turn_off_thumbnails)
+
+    # change back
+    os.chdir(osu_song_path)
+
 
     print("Backup your song folder before using this script...")
     contains_anime_tag = []
@@ -89,20 +110,19 @@ def main():
         if os.path.isdir(dirname):
             for i, file in enumerate(os.listdir(dirname)):
                 filename, extension = splitext(file)
-                if is_background(osu_path + '\\' + dirname + '\\' + file):
+                if is_background(osu_song_path + '\\' + dirname + '\\' + file):
                     if extension == ".jpg":
                         # delete_original(osu_path + '\\' + dirname + '\\' + filename)
                         replace_file(get_background(nature_folder, ".jpg"),
-                                     osu_path + '\\' + dirname + '\\' + file)  # makes sure is jpg
+                                     osu_song_path + '\\' + dirname + '\\' + file)  # makes sure is jpg
                     elif extension == ".png":
                         replace_file(get_background(nature_folder, ".png"),
-                                     osu_path + '\\' + dirname + '\\' + file)  # makes sure is png
+                                     osu_song_path + '\\' + dirname + '\\' + file)  # makes sure is png
                     
-
-
 
 # check if song is tagged as anime
 # tv, anime, japanese
-# see if beatmap backgrouns is supposexc to be a png from the .osu file. If it is then convert the file to a png or change the beatmap file.
+# see if beatmap backgrounds is supposed to be a png from the .osu file.
+# If it is then convert the file to a png or change the beatmap file.
 
 main()
